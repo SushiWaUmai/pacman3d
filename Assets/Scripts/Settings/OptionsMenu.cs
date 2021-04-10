e3c8f9e92901e278aeacc03e50c11ea098aacc50
SushiWaUmai <eugene.bb1@gmail.com>
2021-04-10 13:29:12 +0200

    added option menu

---
+++ Assets/Scripts/Settings/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    private const string masterVolume = "MasterVolume";
+    private const string musicVolume = "MusicVolume";
+    private const string sfxVolume = "SfxVolume";
+
+    private void ChangeVolume(string parameter, float val) => audioMixer.SetFloat(parameter, Mathf.Log10(val) * 20);
+
+    public void ChangeMasterVolume(float val) => ChangeVolume(masterVolume, val);
+    public void ChangeMusicVolume(float val) => ChangeVolume(musicVolume, val);
+    public void ChangeSFXVolume(float val) => ChangeVolume(sfxVolume, val);
+}
